8cc1341ad068f4ef401c966c0eb095a10ab54c53
Thalia-Dev07 <thaliaoficial7@gmail.com>
2022-12-03 22:21:27 -0300

    first commit

---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDatail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDatail.id
+    pokemon.name = pokeDatail.name
+
+    const types = pokeDatail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDatail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetail) => pokemonsDetail)
+}
